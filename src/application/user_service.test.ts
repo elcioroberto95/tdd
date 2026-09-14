c70b9b342de5b1bb75ba8ef1d6b6bdc26f76e983
Elcio Pereira <elcioroberto1995@gmail.com>
2026-09-14 20:57:19 -0300

    test: adding test to function createUser

--- src/application/user_service.test.ts
+++ src/application/user_service.test.ts
@@ -1,5 +1,6 @@
 import { FakeUserRepository } from '../infrastracture/repositories/FakeUserRepository';
 import { UserService } from './user_service';
+import { User } from '../domain/entities/user';
 
 
 
@@ -17,11 +18,18 @@ describe('UserService', () => {
         const user = await userService.getUserById('invalid-id');
         expect(user).toBeNull();
     })
-
-
     it('should return a valid user when a valid id is provided', async () => {
         const user = await userService.getUserById('1');
         expect(user).not.toBeNull();
         expect(user?.getId()).toBe('1');
+    })
+
+
+    it('should be able to create a new user', async () => {
+        const newUser = new User('3', 'Bob Johnson', 'bob@example.com');
+        await userRepository.create(newUser);
+        const user = await userService.getUserById('3');
+        expect(user).not.toBeNull();
+        expect(user?.getId()).toBe('3');
     })
 })
